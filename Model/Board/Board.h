c75f10faf5f31f22985e068a8c89c83f172828ab
ZakITx <113520775+ZakITx@users.noreply.github.com>
2023-06-14 13:32:05 +0200

    Create Board.h

---
+++ Model/Board/Board.h
@@ -0,0 +1,15 @@
+//
+// Created by alexa on 15/05/2023.
+//
+
+#ifndef SCHOTTENT_TOTTEN_BOARD_H
+#define SCHOTTENT_TOTTEN_BOARD_H
+
+namespace Model{
+    class Board {
+
+    };
+}
+
+
+#endif //SCHOTTENT_TOTTEN_BOARD_H
